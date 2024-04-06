3f50b962e9ab81d002d432406470cf246989461a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-04-06 00:24:52 -0400

    patch

--- src/ui/Supply.svelte
+++ src/ui/Supply.svelte
@@ -23,14 +23,14 @@
         </p>
         <p>
           Slow Supply: {$supplyStore.slow.toLocaleString() ?? 0}
-          ({(($supplyStore.slow / $supplyStore.total) * 100).toFixed(0)}% of total) ({(
+          ({(($supplyStore.slow / $supplyStore.total) * 100).toFixed(1)}% of total) ({(
             ($supplyStore.slow / $supplyStore.user) *
             100
           ).toFixed(0)}% of user)
         </p>
         <p>
           Circulating Supply: {$supplyStore.circulating.toLocaleString() ?? 0}
-          ({(($supplyStore.circulating / $supplyStore.total) * 100).toFixed(0)}% of total) ({(
+          ({(($supplyStore.circulating / $supplyStore.total) * 100).toFixed(1)}% of total) ({(
             ($supplyStore.circulating / $supplyStore.user) *
             100
           ).toFixed(0)}% of user)
